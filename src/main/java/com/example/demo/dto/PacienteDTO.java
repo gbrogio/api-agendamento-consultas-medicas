62dc75f8c1bd876aba1c22442a61cb27fe4d8218
antonio neto <antonioneetoo19@gmail.com>
2025-05-10 15:03:22 -0300

    PacienteDTO

---
+++ src/main/java/com/example/demo/dto/PacienteDTO.java
@@ -0,0 +1,43 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class PacienteDTO {
+    
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O CPF é obrigatório")
+    @Size(min = 11, max = 11, message = "O CPF deve ter 11 caracteres")
+    private String cpf;
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "O e-mail é obrigatório")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String senha;
+
+    @NotNull(message = "A data de nascimento é obrigatória")
+    @Size(min = 10, max = 10, message = "A data de nascimento deve ter o formato dd/MM/yyyy")
+    @Past(message = "A data de nascimento deve ser uma data passada")
+    private LocalDate dataNascimento;
+
+    @NotBlank(message = "O telefone é obrigatório")
+    @Size(min = 10, max = 15, message = "O telefone deve ter entre 10 e 15 caracteres")
+    private String telefone;
+}
