b12703b4c7607fc7ec3a49335f0088fe5610de67
antonio neto <antonioneetoo19@gmail.com>
2025-05-14 13:24:54 -0300

    removendo declaração de localdate do pacienterepository e subindo o mapper paciente

--- src/main/java/com/example/demo/mapper/PacienteMapper.java
+++ src/main/java/com/example/demo/mapper/PacienteMapper.java
@@ -10,8 +10,6 @@ import com.example.demo.dto.PacienteDTO;
 @Mapper(componentModel = "spring")
 public interface PacienteMapper {
     PacienteDTO toDTO(Paciente Paciente);
-
     Paciente toEntity(PacienteDTO PacienteDTO);
-
     List<PacienteDTO> toDTOList(List<Paciente> pacientes);
 }
