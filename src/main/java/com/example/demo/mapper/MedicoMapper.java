6b03d297dee3b83b0ef4c16319e76992bc11eaf6
Matheus Moreira <matheusgmoreira1@gmail.com>
2025-05-14 22:11:56 -0300

    mapper

--- src/main/java/com/example/demo/mapper/MedicoMapper.java
+++ src/main/java/com/example/demo/mapper/MedicoMapper.java
@@ -1,8 +1,17 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+
+import com.example.demo.Entities.Medico;
+import com.example.demo.dto.MedicoDTO;
 
 @Mapper(componentModel = "spring")
 public interface MedicoMapper {
-    
+    MedicoDTO toDTO(Medico Medico);
+
+    Medico toEntity(MedicoDTO MedicoDTO);
+
+    List<MedicoDTO> toDTOList(List<Medico> medicos);
 }
