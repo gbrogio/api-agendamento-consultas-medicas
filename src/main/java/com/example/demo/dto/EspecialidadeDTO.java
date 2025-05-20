3e3e55d1ac9829df4ce3c63c7b684c9ce2e99300
Leonardo Campelo Ribeiro <leonardocampeloribeiro@gmail.com>
2025-05-20 16:56:50 -0300

    Especialidade

---
+++ src/main/java/com/example/demo/dto/EspecialidadeDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EspecialidadeDTO {
+
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
+    private String nome;
+
+}
