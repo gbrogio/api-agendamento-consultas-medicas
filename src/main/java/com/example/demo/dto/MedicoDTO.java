9a435903ce96efa723337433db48b906bf765641
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2025-05-20 18:04:17 -0300

    Implementações do Matheus

---
+++ src/main/java/com/example/demo/dto/MedicoDTO.java
@@ -0,0 +1,36 @@
+package com.example.demo.dto;
+
+//import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Class feita para a validação dos atributos da classe Médico
+
+@Data
+@NoArgsConstructor
+public class MedicoDTO {
+
+    private Long id;
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
+    private String nome;
+
+    /*@NotNull(message = "A data de nascimento é obrigatória")
+    @Size(min = 10, max = 10, message = "A data de nascimento deve ter o formato dd/MM/yyyy")
+    @Past(message = "A data de nascimento deve ser uma data passada")
+    private LocalDate dataNascimento;*/
+
+    @NotBlank(message = "O CRM é obrigatório")
+    @Size(min = 5, max = 10, message = "O CRM deve ter entre 5 e 10 caracteres")
+    private String crm;
+
+    @NotBlank(message = "A especialidade é obrigatória")
+    @Size(min = 3, max = 50, message = "A especialidade deve ter entre 3 e 50 caracteres")
+    private String especialidadeId;
+}
+
---
+++ src/main/java/com/example/demo/repository/IMedicoRepository.java
@@ -0,0 +1,22 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.Entities.Medico;
+
+@Repository
+public interface IMedicoRepository extends JpaRepository<Medico, Long> {
+    Optional<Medico> findById(Long id);
+
+    List<Medico> findByNome(String nome);
+
+    Optional<Medico> findByCrm(String crm);
+
+    List<Medico> findByEspecialidadeId(Long especialidadeId);
+}
+
+// esse trecho do codigo é o repositório do Medico, que estende a interface
+// JpaRepository
+// ele possui métodos para buscar médicos por id, nome, crm e especialidadeId
