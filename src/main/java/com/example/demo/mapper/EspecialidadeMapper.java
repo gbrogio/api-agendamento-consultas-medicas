dd6de8a933955b43eced2fc06278c6da230e57d2
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2025-05-20 18:08:09 -0300

    Implementações Leonardo

---
+++ src/main/java/com/example/demo/mapper/EspecialidadeMapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.example.demo.Entities.Especialidade;
+import com.example.demo.dto.EspecialidadeDTO;
+
+@Mapper(componentModel = "spring")
+public interface EspecialidadeMapper {
+    EspecialidadeDTO toDTO(Especialidade especialidade);
+
+    Especialidade toEntity(EspecialidadeDTO especialidadeDTO);
+
+    List<EspecialidadeDTO> toDTOList(List<Especialidade> especialidades);
+}
