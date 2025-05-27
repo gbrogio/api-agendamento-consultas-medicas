f7c1228eac4fa8e79e2cebae488d8b92c84a6db5
Matheus Moreira <matheusgmoreira1@gmail.com>
2025-05-27 17:35:08 -0300

    dto

--- src/main/java/com/example/demo/dto/MedicoDTO.java
+++ src/main/java/com/example/demo/dto/MedicoDTO.java
@@ -16,20 +16,11 @@ import lombok.NoArgsConstructor;
 public class MedicoDTO {
 
     private Long id;
+    private boolean ativo = true;
 
     @NotBlank(message = "O nome é obrigatório")
     @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
     private String nome;
-
-    /*
-     * @NotNull(message = "A data de nascimento é obrigatória")
-     * 
-     * @Size(min = 10, max = 10, message =
-     * "A data de nascimento deve ter o formato dd/MM/yyyy")
-     * 
-     * @Past(message = "A data de nascimento deve ser uma data passada") private
-     * LocalDate dataNascimento;
-     */
 
     @NotBlank(message = "O CRM é obrigatório")
     @Size(min = 5, max = 10, message = "O CRM deve ter entre 5 e 10 caracteres")
